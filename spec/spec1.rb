2a3bc9bf65d2ef1398e491ea559a6356b7607499
root <zoxplers@gmail.com>
2022-11-11 03:41:59 +0000

    AYAYA RSPEC

--- spec/spec1.rb
+++ spec/spec1.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "If the numbers below match, rspec and ruby are installed and working normally" do
-  it "should equal 42" do
-    expect(6 * 7).to eq(42)
-  end
+    it "should equal 42" do
+        expect(6 * 7).to eq(42)
+    end
+end
+
+RSpec.describe "This next test should show an error" do
+    it "should equal 42 but we said 43" do
+        expect(6 * 7).to eq(43)
+    end
 end
